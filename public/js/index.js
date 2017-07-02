f79e81d842a37420ca1fef07258ebab31b418ff7
Mike Wight <michel.wight@hotmail.com>
2017-07-02 12:38:03 -0300

    Emit new message on create message

--- public/js/index.js
+++ public/js/index.js
@@ -3,12 +3,6 @@ let socket = io();
 socket.on('connect', function () {
     console.log('Connected to server');
 
-    socket.emit('createMessage', {
-        from : 'Mike',
-        text: 'Sup dawg'
-    });
-
-
 });
 
 
