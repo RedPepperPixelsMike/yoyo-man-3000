9fe10ad0dfb02bccfb1ed9b7092f623a77936bd0
Mike Wight <michel.wight@hotmail.com>
2017-07-03 00:23:36 -0300

    Send message to only poeple in same room

--- public/js/chat.js
+++ public/js/chat.js
@@ -80,7 +80,6 @@ $('#message-form').on('submit', function (e) {
     let messageTextbox = jQuery('[name=message]');
 
     socket.emit('createMessage', {
-        from: 'User',
         text: messageTextbox.val()
     }, function () {
         messageTextbox.val('');
--- server/server.js
+++ server/server.js
@@ -45,11 +45,14 @@ io.on('connection', (socket) => {
 
 
     socket.on('createMessage', (message, callback) => {
-        console.log('create message', message);
+        let user = users.getUser(socket.id);
 
-        io.emit('newMessage', generateMessage(
-            message.from, message.text
-        ));
+        if (user && isRealString(message.text)){
+            io.to(user.room).emit('newMessage', generateMessage(
+                user.name, message.text
+            ));
+        }
+
         callback('This is from the server.');
         // socket.broadcast.emit('newMessage', {
         //     from: message.from,
@@ -59,7 +62,13 @@ io.on('connection', (socket) => {
     });
 
     socket.on('createLocationMessage', (coords) => {
-       io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude))
+        let user = users.getUser(socket.id);
+
+        if (user) {
+            io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name, coords.latitude, coords.longitude))
+
+        }
+
     });
 
     socket.on('disconnect', () => {
